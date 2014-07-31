83595f5b64e52ba522c918f6f14fbf383efebd3a
Franklin Javier <franklinjalves@gmail.com>
2014-07-31 09:44:02 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+
+    var demo = new Vue({
+        el: '#nostalgia',
+        data: {
+            videos: videos
+        },
+        created: function () {
+            this.$watch('year', function () {
+                this.fetchData();
+            });
+        },
+        methods: {
+            fetchData: function () {    
+                
+                if ( ! this.year || this.year === 'undefined' ) {
+                    this.display = 'hide';
+                    return false;
+                }
+
+                var year = this.year,
+                    items = this.$data.videos[ year ],
+                    i = 0,
+                    len = items.length,
+                    ids = '';
+            
+                for ( ; i < len; i++ ) {
+                    ids += items[ i ].id + 
+                        (i === len-1 ? '' : ',');
+                }
+            
+                this.id = ids;
+                this.display = 'show';
+            }
+        }
+    })
+
+};
